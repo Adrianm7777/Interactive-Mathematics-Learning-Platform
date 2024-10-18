efd176a22ccacb98edcc37d7dde54eae54b5a1ed
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-18 16:01:58 +0200

    feature: Created Data Models

--- backend/math_learning_platform/math_app/models.py
+++ backend/math_learning_platform/math_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Problem(models.Model):
+    question = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.question
+    
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    user_answer = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.problem.question}"
+    
+class Progress(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    completed_problem = models.IntegerField(default=0)
+    correct_answers = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username}'s Progress"
