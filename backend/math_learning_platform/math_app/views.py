4010d8ca6271a206fad9cc93f7bed95586c4c6ff
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-21 17:33:27 +0200

    feature: Created Views for the API

--- backend/math_learning_platform/math_app/views.py
+++ backend/math_learning_platform/math_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Answer,Problem,Progress
+from .serializers import AnswerSerializer,ProblemSerializer,ProgressSerializer
 
-# Create your views here.
+class ProblemViewSet(viewsets.ModelViewSet):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class =AnswerSerializer
+
+class ProgressViewSet(viewsets.ModelViewSet):
+    queryset = Progress.objects.all()
+    serializer_class = ProgressSerializer
