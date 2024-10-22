d42466b67261c2c50341d18a76243d32a9e5d740
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-22 17:18:36 +0200

    feature: Defined Fetch Functions in api.ts

---
+++ frontend/math-learning-frontend/services/api.ts
@@ -0,0 +1,36 @@
+import { BASE_URL } from "./endpoints";
+
+export const getMathProblem = async () => {
+  const response = await fetch(`${BASE_URL}/problems/`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch math problems");
+  }
+
+  return response.json();
+};
+
+export const submitAnswer = async (problemId: number, userAnswer: string) => {
+  const response = await fetch(`${BASE_URL}/answers/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      problem: problemId,
+      user_answer: userAnswer,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to submit answer");
+  }
+  return response.json();
+};
+
+export const getUserProgess = async () => {
+  const response = await fetch(`${BASE_URL}/progress/`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch user progress");
+  }
+  return response.json();
+};
