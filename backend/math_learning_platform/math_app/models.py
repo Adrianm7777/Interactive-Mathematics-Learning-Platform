e4c6480946be09d15797d0f81b30c4d90167265f
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-23 15:50:44 +0200

    fix: added function to check anwer is correct and deleted user from model

--- backend/math_learning_platform/math_app/models.py
+++ backend/math_learning_platform/math_app/models.py
@@ -10,13 +10,16 @@ class Problem(models.Model):
         return self.question
     
 class Answer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     user_answer = models.CharField(max_length=100)
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.user.username} - {self.problem.question}"
+        return f"{self.problem.question} - {self.user_answer}"
+    
+    def save(self, *args, **kwargs):
+        self.is_correct = self.user_answer.strip() == self.problem.correct_answer.strip()
+        super().save(*args, **kwargs)
     
 class Progress(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
