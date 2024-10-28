110c06a98cf74b24d7005be0b3604bea10347372
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-28 16:21:21 +0100

    fix: deleted progress form urls

--- backend/math_learning_platform/math_app/urls.py
+++ backend/math_learning_platform/math_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AnswerViewSet,ProblemViewSet,ProgressViewSet
+from .views import AnswerViewSet,ProblemViewSet
 
 router = DefaultRouter()
 router.register(r'problems', ProblemViewSet)
 router.register(r'answers', AnswerViewSet)
-router.register(r'progress', ProgressViewSet)
 
 
 urlpatterns = [
