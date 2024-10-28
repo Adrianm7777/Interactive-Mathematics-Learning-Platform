9170d95734120de83840e90cf60463494730e1ac
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-28 16:20:31 +0100

    fix: deleted progress model

--- backend/math_learning_platform/math_app/models.py
+++ backend/math_learning_platform/math_app/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Problem(models.Model):
     question = models.CharField(max_length=255)
     correct_answer = models.CharField(max_length=100)
-    difficulty = models.CharField(max_length=50)
 
     def __str__(self):
         return self.question
@@ -21,11 +19,3 @@ class Answer(models.Model):
         self.is_correct = self.user_answer.strip() == self.problem.correct_answer.strip()
         super().save(*args, **kwargs)
     
-class Progress(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    completed_problem = models.IntegerField(default=0)
-    correct_answers = models.IntegerField(default=0)
-    current_difficulty = models.CharField(max_length=50, default='easy')
-
-    def __str__(self):
-        return f"{self.user.username}'s Progress"
